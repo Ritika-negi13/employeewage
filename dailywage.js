6da95658c4ff7907bbff4b4c1248132772869a9f
Ritika Negi <ritikanegi1013@gmail.com>
2024-07-22 23:18:30 +0530

    use case 2: daily wage calculation

---
+++ dailywage.js
@@ -0,0 +1,14 @@
+let hours=8, wagePerHour=20;
+function calDailyWage( employeeAttendance){
+    if(employeeAttendance==0)
+    {
+        return 0;
+    }
+    return hours*wagePerHour;
+}
+
+module.exports={
+    calDailyWage:calDailyWage
+}
+
+
--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("hello")
 
 let attendance=require('./employeeattendance')
 console.log(attendance.calAttendance()==1?"Present":"Absent");
+
+let dailywage =require('./dailywage')
+console.log(dailywage.calDailyWage(employeeAttendance));
